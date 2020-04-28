80a1155b3cde5927ac186fc52fc303bef75d8f67
zhi_shui <zhi_shui@hunliji.com>
2020-04-28 15:13:50 +0800

    init

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,22 +1,5 @@
 import React from 'react'
-import { App, Sidebar } from 'react-admin-core'
 
 export default function ({ children }) {
-  return (
-    <App
-      isLogin={() => false}
-      sidebar={
-        <Sidebar
-          config={[
-            {
-              title: '主页',
-              page: 'index',
-            },
-          ]}
-        />
-      }
-    >
-      {children}
-    </App>
-  )
+  return <div>{children}</div>
 }
